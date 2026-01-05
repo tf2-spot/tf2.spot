f167938f9f5b43ec8d642286b4b4f566f3c78cc4
ldesgoui <ldesgoui@gmail.com>
2026-01-05 15:46:22 +0100

    make parse-manifests more safe

--- pkgs/parse-manifests/default.nix
+++ pkgs/parse-manifests/default.nix
@@ -1,19 +1,20 @@
-{ writeShellApplication, jq }:
+{ writeShellApplication, eza, jq }:
 writeShellApplication {
   name = "parse-manifests";
 
-  runtimeInputs = [ jq ];
+  runtimeInputs = [ eza jq ];
   runtimeEnv = {
     ASSETS_FILELIST = ./filelist-assets-no-vpk.txt;
     PARSE_MANIFESTS = ./parse-manifests.jq;
   };
   text = ''
+    newest() { eza --sort=time -1 "$@" | head -1; }
     jq \
       --null-input \
       --rawfile assets_filelist $ASSETS_FILELIST \
-      --rawfile assets  manifests/manifest_232250_*.txt \
-      --rawfile windows manifests/manifest_232255_*.txt \
-      --rawfile linux   manifests/manifest_232256_*.txt \
+      --rawfile assets  "$(newest manifests/manifest_232250_*.txt)" \
+      --rawfile windows "$(newest manifests/manifest_232255_*.txt)" \
+      --rawfile linux   "$(newest manifests/manifest_232256_*.txt)" \
       --from-file $PARSE_MANIFESTS
   '';
 }
