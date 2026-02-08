69f89eab67c694b69a4d1658769f77bfc09a7362
ldesgoui <ldesgoui@gmail.com>
2026-02-08 03:24:26 +0100

    uppies

--- sqitch/fantasy/deploy/player_performance.sql
+++ sqitch/fantasy/deploy/player_performance.sql
@@ -4,19 +4,89 @@ begin;
 
 set search_path to fantasy;
 
--- create table player_performance
--- ( map              int not null
--- , player           text not null
--- , player_statistic text not null
--- , value            decimal not null
--- , primary key (map, player, player_statistic)
--- , foreign key (map) references map
--- , foreign key (player) references player
--- , foreign key (player_statistic) references player_statistic
--- );
-
 create materialized view player_performance as
-select 1;
+with pre as (
+  select participant.id as participant
+       , any_value(tournament.id) as tournament
+       , any_value(round.id) as round
+       , any_value(match.id) as match
+       , map.id as map
+       , coef.id as player_coefficient
+       , case when highest and divide_by is not null
+              then ( sum(var.value) / greatest(sum(div.value), 1)
+                     = max(sum(var.value) / greatest(sum(div.value), 1)) over (partition by map.id, coef.id)
+                   )::int::decimal
+
+              when lowest and divide_by is not null
+              then ( sum(var.value) / greatest(sum(div.value), 1)
+                     = min(sum(var.value) / greatest(sum(div.value), 1)) over (partition by map.id, coef.id)
+                   )::int::decimal
+
+              when highest
+              then ( sum(var.value) = max(sum(var.value)) over (partition by map.id, coef.id)
+                   )::int::decimal
+
+              when lowest
+              then ( sum(var.value) = min(sum(var.value)) over (partition by map.id, coef.id)
+                   )::int::decimal
+
+              when divide_by is not null
+              then sum(var.value) / greatest(sum(div.value), 1)
+
+              else sum(var.value)
+         end as total
+       , coef.score_coefficient
+       , divide_by is not null and not highest and not lowest as is_average
+       , sum(var.value) as variable
+       , sum(div.value) as divide_by
+
+  from map
+  join match on match.id = map.match
+  join round on round.id = match.round
+  join tournament on tournament.id = round.tournament
+  join scoring_model on scoring_model.id = tournament.scoring_model
+  join player_coefficient coef on coef.scoring_model = scoring_model.id
+
+  join team on team.id in (match.team_a, match.team_b)
+  join participant on participant.team = team.id
+  left join substitution on substitution.map = map.id and substitution.participant = participant.id
+  join player on player.steam_id in (participant.player, substitution.substitute)
+  
+  left join extract_player_stats var
+    on var.log_id in (map.log_id, map.golden_cap_log_id)
+    and var.steam_id = player.steam_id
+    and var.statistic = coef.variable
+  
+  left join extract_player_stats div
+    on div.log_id in (map.log_id, map.golden_cap_log_id)
+    and div.steam_id = player.steam_id
+    and div.statistic = coef.divide_by
+
+  group by (map.id, coef.id, participant.id)
+)
+
+select participant
+     , tournament
+     , round
+     , match
+     , map
+     , player_coefficient
+     , case when grouping(player_coefficient) = 1 then null
+            when any_value(is_average) then sum(variable)/greatest(sum(divide_by), 1)
+            else sum(total)
+       end as total
+     , sum(total * score_coefficient) as score
+from pre
+group by grouping sets
+( (participant, tournament, round, match, map, player_coefficient)
+, (participant, tournament, round, match, map)
+, (participant, tournament, round, match, player_coefficient)
+, (participant, tournament, round, match)
+, (participant, tournament, round, player_coefficient)
+, (participant, tournament, round)
+, (participant, tournament, player_coefficient)
+, (participant, tournament)
+);
 
 comment on materialized view player_performance is 'how much of a statistic has a player achieved';
 
