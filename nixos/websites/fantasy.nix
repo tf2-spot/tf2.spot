f44c1af5201d67617784e70313ee37737fde9000
ldesgoui <ldesgoui@gmail.com>
2026-07-19 03:31:57 +0200

    nixos/websites: run fantasy-websites in a service

--- nixos/websites/fantasy.nix
+++ nixos/websites/fantasy.nix
@@ -1,14 +1,28 @@
-{ lib, config, ... }:
+{ lib, pkgs, config, ... }:
 let
   inherit (lib) types mkOption mkEnableOption mkIf;
 
   cfg = config.tf2-spot;
+
+  gunicornConfig = pkgs.writeText "gunicorn.conf.py" ''
+    import multiprocessing
+
+    bind = ['[::1]:8180']
+    proxy_protocol = 'v2'
+
+    workers = multiprocessing.cpu_count() * 2 + 1
+    preload_app = True
+  '';
 in
 {
   options = {
     tf2-spot = {
       fantasy = {
         enable = mkEnableOption "";
+
+        package = mkOption {
+          type = types.raw;
+        };
 
         domain = mkOption {
           type = types.str;
@@ -31,9 +45,31 @@ in
         "http${if cfg.tls then "s" else ""}://${cfg.fantasy.domain}" = {
           useACMEHost = mkIf cfg.tls "${cfg.fantasy.domain}";
           extraConfig = ''
-            respond 500
+            reverse_proxy {
+              to http://localhost:8180
+              transport http {
+                proxy_protocol v2
+              }
+            }
           '';
         };
+      };
+    };
+
+    systemd.services.fantasy-website = {
+      wantedBy = [ "multi-user.target" ];
+      after = [ "network.target" ];
+
+      serviceConfig = {
+        ExecStart = "${cfg.fantasy.package}/bin/gunicorn fantasy_website:app --config ${gunicornConfig}";
+        ExecReload = "${pkgs.coreutils}/bin/kill -s HUP $MAINPID";
+
+        Type = "notify";
+        NotifyAccess = "main";
+        TimeoutStopSec = "5s";
+        KillMode = "mixed";
+
+        DynamicUser = true;
       };
     };
   };
