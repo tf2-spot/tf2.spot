d39d6aa28aff8f2de79ed790762ee6d12ed227a0
ldesgoui <ldesgoui@gmail.com>
2026-07-22 03:17:59 +0200

    sqitch/fantasy: fix revert for fantasy_janitor role

--- sqitch/fantasy/revert/role-janitor.sql
+++ sqitch/fantasy/revert/role-janitor.sql
@@ -2,6 +2,13 @@
 
 begin;
 
+revoke select, update on fantasy.manager from fantasy_janitor;
+revoke select on fantasy.map from fantasy_janitor;
+revoke select, insert on fantasy.logstf_document from fantasy_janitor;
+revoke maintain on fantasy.player_performance from fantasy_janitor;
+revoke maintain on fantasy.team_performance from fantasy_janitor;
+revoke usage on schema fantasy from fantasy_janitor;
+
 drop role fantasy_janitor;
 
 commit;
